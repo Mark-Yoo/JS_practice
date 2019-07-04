b9934682eaa3f33aba9b2bf10494cb0bb48073a2
Mark Yoo <yjinhyuk@gmail.com>
2019-07-04 19:38:56 +0900

    Feat: Angular-template-driven example added

---
+++ Angular-template-driven/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <app-user-form></app-user-form>
+  `,
+  styles: []
+})
+export class AppComponent {
+}
